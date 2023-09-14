f72389b4d8fe9c7849d368231993226773c23be4
venus <99412095+venous666@users.noreply.github.com>
2023-09-14 09:31:32 -0300

    Add files via upload

---
+++ src/test/java/com/livro/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livro.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
